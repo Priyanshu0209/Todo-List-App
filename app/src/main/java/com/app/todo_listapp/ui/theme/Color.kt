344c92e20dc943b23f74b6b70f98b845c3c68664
navneet <navneetpalgithub@gmail.com>
2024-07-20 17:38:47 +0530

    Priyanshu

--- app/src/main/java/com/app/todo_listapp/ui/theme/Color.kt
+++ app/src/main/java/com/app/todo_listapp/ui/theme/Color.kt
@@ -14,3 +14,11 @@ val Pink40 = Color(0xFF7D5260)
 val SystemBackgroundColor = Color(0xFF252525)
 val IconButtonColor = Color(0xFF3B3B3B)
 val textHintTitleColor = Color(0xFF9A9A9A)
+
+// Note Colors
+val noteColor1 = Color(0xFFFD99FF)
+val noteColor2 = Color(0xFFFF9E9E)
+val noteColor3 = Color(0xFF91F48F)
+val noteColor4 = Color(0xFFFFF599)
+val noteColor5 = Color(0xFF9EFFFF)
+val noteColor6 = Color(0xFFB69CFF)
